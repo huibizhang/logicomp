f87818516ff825be35f23a4e3c57cd169fabfd38
搋兔 <naruto6404@hotmail.com>
2023-11-27 01:34:48 +0800

    reanme: merge example and pacakge/logicomp

--- packages/logicomp/src/App.vue
+++ packages/logicomp/src/App.vue
@@ -2,19 +2,24 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
+import { ref } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue";
+
+import { InputField } from "logicomp";
+
+const msg = ref("Vite + Vue 3 + Typescript + Tailwindcss v3");
 </script>
 
 <template>
   <div class="text-center selection:bg-green-100">
     <img class="mx-auto mb-8 mt-12" alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Vite + Vue 3 + Typescript + Tailwindcss v3" />
+    <HelloWorld :msg="msg" />
     <img
       class="mx-auto my-8"
       alt="powered-by"
       src="./assets/powered-by-vitawind-bright.png"
     />
-    <LogiButton class="border">123</LogiButton>
+    <InputField name="title" type="text" v-model="msg"></InputField>
   </div>
 </template>
 
---
+++ packages/logicomp/src/components/Form/InputField.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+type modelValue = string | number;
+
+const props = withDefaults(
+  defineProps<{
+    name?: string;
+    type?: "text" | "number" | "range";
+    modelValue: modelValue;
+  }>(),
+  {
+    name: "",
+    type: "text",
+    modelValue: "",
+  }
+);
+
+const emit = defineEmits<{
+  (event: "update:modelValue", value: modelValue): void;
+}>();
+
+const update = (evt: Event) => {
+  let val: modelValue = (evt.target as HTMLInputElement)?.value;
+  const isNum = ["number", "range"].includes(props.type);
+  emit("update:modelValue", isNum ? Number(val) : val);
+};
+</script>
+
+<template>
+  <input :name="name ?? ''" :type="type" :value="modelValue" @input="update" />
+</template>
